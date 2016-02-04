2c00d5b17560838e97a2632a656268277a79281b
infinitumnihilum <jrdavis1989@gmail.com>
2016-02-04 14:14:19 -0500

    changed snowslection for user input

--- src/main/java/davis/john/SnowSelection.java
+++ src/main/java/davis/john/SnowSelection.java
@@ -1,11 +1,14 @@
 package davis.john;
 
+import java.util.Scanner;
+
 /**
  * Created by jrdavis on 2/4/16.
  */
 public class SnowSelection {
 
     String snowType = userType;
+    Scanner userType;
 
 
 }
